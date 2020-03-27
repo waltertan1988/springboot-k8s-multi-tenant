7fd44299eb458a8239a9bd9e34b1a56316f839f4
tanyunxing <tanyunxing@yy.com>
2020-03-27 17:00:49 +0800

    ping打印ip地址和主机名

--- src/main/java/org/walter/base/controller/PingController.java
+++ src/main/java/org/walter/base/controller/PingController.java
@@ -3,11 +3,15 @@ package org.walter.base.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class PingController {
 
     @GetMapping("/ping")
-    public String ping(){
-        return "Success!";
+    public String ping() throws UnknownHostException {
+        InetAddress addr = InetAddress.getLocalHost();
+        return String.format("Success(%s, %s)", addr.getHostName(), addr.getHostAddress());
     }
 }

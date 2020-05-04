bfe4e39acc22810856bcc837c1298127b69c9f35
tanyunxing <tanyunxing@yy.com>
2020-05-04 09:25:38 +0800

    后端加入一些测试方法

--- src/main/java/org/walter/base/controller/TestController.java
+++ src/main/java/org/walter/base/controller/TestController.java
@@ -1,6 +1,8 @@
 package org.walter.base.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
@@ -15,5 +17,17 @@ public class PingController {
     public String ping() throws UnknownHostException {
         InetAddress addr = InetAddress.getLocalHost();
         return String.format("[Success] hostname=%s, address=%s, appVersion=%s\n", addr.getHostName(), addr.getHostAddress(), APP_VERSION);
+    }
+
+    @GetMapping("/delay/{timeout}")
+    public String delay(@PathVariable("timeout") long timeout) throws InterruptedException {
+        Thread.sleep(timeout);
+        return String.format("[Delay] %s ms", timeout);
+    }
+
+    @GetMapping("/fail")
+    @ResponseStatus
+    public Boolean fail() {
+        return true;
     }
 }
